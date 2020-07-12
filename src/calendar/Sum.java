4be6ad25c15e4ad68d2e6a02d04c33aeb8976d23
Tezla-Lee <tezla.lee@gmail.com>
2020-07-12 15:30:21 +0900

    Add method

--- src/calendar/Sum.java
+++ src/calendar/Sum.java
@@ -1,22 +1,32 @@
 package calendar;
+
 import java.util.Scanner;
+
 public class Sum {
 
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("두 수를 입력하세요.");
-//		int a = scanner.nextInt();
-//		int b = scanner.nextInt();
 		
-		String num = scanner.nextLine();
-		String [] SplitedValue = num.split(" ");
+		// 방법 1
+		String s1 = scanner.next();
+		String s2 = scanner.next();
 		
-		int a = Integer.parseInt(SplitedValue[0]);
-		int b = Integer.parseInt(SplitedValue[1]);
+		int a = Integer.parseInt(s1);
+		int b = Integer.parseInt(s2);
+		
+		// 방법 2
+//		String num = scanner.nextLine();
+//		String [] SplitedValue = num.split(" ");
+		
+//		int a = Integer.parseInt(SplitedValue[0]);
+//		int b = Integer.parseInt(SplitedValue[1]);
 		
 		
-		System.out.println("두 수의 합은 " + (a+b) + "입니다.");
+//		System.out.println("두 수의 합은 " + (a+b) + "입니다.");
+//		System.out.printf("두 수의 합은 %d입니다.", a + b);
+		System.out.printf("%d와 %d의 합은 %d입니다.", a, b, a + b);
 	}
 
 }
