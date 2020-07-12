b2ff2191242cabf328694c37cae09c715cbf776c
Tezla-Lee <tezla.lee@gmail.com>
2020-07-12 15:05:32 +0900

    Update calendar

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -5,6 +5,22 @@ public class Calendar {
 	public static void main(String[] args) {
 
 		System.out.println("Hello, Calendar");
+		String s;
+		
+		System.out.println(" 일  월  화  수  목  금  토");
+		System.out.println("--------------------------");
+
+		for (int day = 1; day <= 31; day++) {
+			s = Integer.toString(day);
+			if (s.length() == 1) {
+				System.out.print(" ");
+			}
+			System.out.print(day);
+			System.out.print("  ");
+			if (day % 7 == 0) {
+				System.out.println();
+			}
+		}
 	}
 
 }
