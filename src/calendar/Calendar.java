a7c586394d433c39d359793d07b2b79217613435
Tezla-Lee <tezla.lee@gmail.com>
2020-07-12 16:43:33 +0900

    using 'while'

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -48,14 +48,33 @@ public class Calendar {
 //		System.out.printf("%d월은 %d일까지 있습니다.", month, maxDays[month - 1]);
 //		
 		// 반복 입력, 입력 달만 출력
+		String prompt = "cal>";
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("반복 횟수를 입력하세요.");
-		int repeat = scanner.nextInt();
+//		System.out.println("반복 횟수를 입력하세요.");
+//		int repeat = scanner.nextInt();
 
-		for (int i = 0; i < repeat; i++) {
+//		for (int i = 0; i < repeat; i++) {
+//			System.out.println("달을 입력하세요.");
+//			System.out.print(prompt);
+//			int month = scanner.nextInt();
+//			int days = 0;
+//			
+
+//
+//		}
+//		System.out.println("Bye !");
+//		scanner.close();
+//		 
+		
+		int month = 0;
+		int days =0;
+		while (month != -1) {
 			System.out.println("달을 입력하세요.");
-			int month = scanner.nextInt();
-			int days = 0;
+			System.out.print(prompt);
+			month = scanner.nextInt();
+			if ( month == -1) {
+				break;
+			}
 			switch (month) {
 			case 1, 3, 5, 7, 8, 12:
 				days = 31;
@@ -69,10 +88,9 @@ public class Calendar {
 			}
 			System.out.printf("%d월은 %d일까지 있습니다.", month, days);
 			System.out.println();
-
+			System.out.println();
 		}
 		System.out.println("Bye !");
-		scanner.close();
 	}
 
 }
